a12675e1e176ba32a2fc7ef4b94b61a7ea23dd7d
Vladislav Andreev <me@vld.by>
2021-01-18 16:34:44 +0300

    Change regex to capture only numbers

--- speedtest_log/parsed_output.rb
+++ speedtest_log/parsed_output.rb
@@ -1,7 +1,7 @@
 class ParsedOutput
-  LATENCY_REGEXP = %r{Latency:\s+(\d+\.\d+ \w+)}.freeze
-  DOWNLOAD_REGEXP = %r{Download:\s+(\d+\.\d+ \w+)}.freeze
-  UPLOAD_REGEXP = %r{Upload:\s+(\d+\.\d+ \w+)}.freeze
+  LATENCY_REGEXP = %r{Latency:\s+(\d+\.\d+) \w+}.freeze
+  DOWNLOAD_REGEXP = %r{Download:\s+(\d+\.\d+) \w+}.freeze
+  UPLOAD_REGEXP = %r{Upload:\s+(\d+\.\d+) \w+}.freeze
 
   attr_reader :command_output
 
@@ -10,14 +10,14 @@ class ParsedOutput
   end
 
   def download
-    command_output.match(DOWNLOAD_REGEXP)[0]
+    command_output.match(DOWNLOAD_REGEXP)[1].to_f
   end
 
   def upload
-    command_output.match(UPLOAD_REGEXP)[0]
+    command_output.match(UPLOAD_REGEXP)[1].to_f
   end
 
   def latency
-    command_output.match(LATENCY_REGEXP)[0]
+    command_output.match(LATENCY_REGEXP)[1].to_f
   end
 end
