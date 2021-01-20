13d4249d53d1541937024f7ebdec6a2a23f44521
Vladislav Andreev <me@vld.by>
2021-01-20 11:51:43 +0300

    Add latency data

--- web/app/controllers/application_controller.rb
+++ web/app/controllers/application_controller.rb
@@ -24,12 +24,13 @@ class SpeedtestChartDataService
   end
 
   def call
-    chart_data = { labels: [], download: [], upload: [] }
+    chart_data = { labels: [], download: [], upload: [], latency: [] }
 
     speedtest_logs.map do |log|
       chart_data[:labels] << log.timestamp.strftime('%d.%m.%Y %H:%M')
       chart_data[:download] << log.download
       chart_data[:upload] << log.upload
+      chart_data[:latency] << log.latency
     end
 
     chart_data
